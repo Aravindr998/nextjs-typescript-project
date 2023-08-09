3a1bba70ffa706d66705a49668219a247ab1b1c5
Aravind R <aravindr998@gmail.com>
2023-08-10 02:05:22 +0530

    added model for user

---
+++ utils/database.ts
@@ -0,0 +1,23 @@
+import mongoose, { ConnectOptions } from "mongoose"
+
+let isConnected = false
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true)
+  if (isConnected) {
+    console.log("=> using existing database connection")
+    return
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || "", {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions)
+    isConnected = true
+    console.log("=> using new database connection")
+  } catch (error) {
+    console.log(error)
+  }
+}
